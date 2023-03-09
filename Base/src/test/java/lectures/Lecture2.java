0e1ec074a9036404d7ade0ac80bb27d29ed08685
Marcos Daniel Saguier <matecondani@gmail.com>
2023-03-08 22:06:58 -0300

    resolved exercises 1, 2, 3 and 4, on half way to 5

--- Base/src/test/java/lectures/Lecture2.java
+++ Base/src/test/java/lectures/Lecture2.java
@@ -1,29 +1,36 @@
 package lectures;
 
 import beans.Person;
-import java.util.List;
-import java.util.stream.IntStream;
 import mockdata.MockData;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Lecture2 {
 
-  @Test
-  public void range() throws Exception {
+    @Test
+    public void range() throws Exception {
+        IntStream.range(0, 10).forEach(System.out::println);
+        System.out.println();
+        IntStream.rangeClosed(0, 10).forEach(System.out::println);
+    }
 
-  }
+    @Test
+    public void rangeIteratingLists() throws Exception {
+        List<Person> people = MockData.getPeople();
+        IntStream.range(0, people.size())
+                .forEach(index -> {
+                    Person person = people.get(index);
+                    System.out.println(person);
+                });
+    }
 
-  @Test
-  public void rangeIteratingLists() throws Exception {
-    List<Person> people = MockData.getPeople();
-
-  }
-
-  @Test
-  public void intStreamIterate() throws Exception {
-      IntStream.iterate(0, operand -> operand + 1)
-          .filter(number -> number % 2 == 0)
-          .limit(20)
-          .forEach(System.out::println);
-  }
+    @Test
+    public void intStreamIterate() throws Exception {
+        IntStream.iterate(0, operand -> operand + 1)
+                .filter(number -> number % 2 == 0)
+                .limit(20)
+                .forEach(System.out::println);
+    }
 }
--- Base/src/test/java/lectures/Lecture5.java
+++ Base/src/test/java/lectures/Lecture5.java
@@ -1,43 +1,45 @@
 package lectures;
 
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import beans.Car;
 import beans.Person;
-import beans.PersonDTO;
 import com.google.common.collect.ImmutableList;
-import java.util.List;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
 import mockdata.MockData;
 import org.junit.Test;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Lecture5 {
 
-  @Test
-  public void understandingFilter() throws Exception {
-    ImmutableList<Car> cars = MockData.getCars();
+    @Test
+    public void understandingFilter() throws Exception {
+        ImmutableList<Car> cars = MockData.getCars();
+        List<Car> expensiveCars = cars.stream()
+                .filter(car -> car.getPrice() > 50000)
+                .collect(Collectors.toList());
+        System.out.println(expensiveCars);
+        expensiveCars.forEach(System.out::println);
+        System.out.println(expensiveCars.size());
+    }
 
-  }
+    @Test
+    public void ourFirstMapping() throws Exception {
+        // transform from one data type to another
+        List<Person> people = MockData.getPeople();
 
-  @Test
-  public void ourFirstMapping() throws Exception {
-    // transform from one data type to another
-    List<Person> people = MockData.getPeople();
+    }
 
-  }
+    @Test
+    public void averageCarPrice() throws Exception {
+        // calculate average of car prices
 
-  @Test
-  public void averageCarPrice() throws Exception {
-    // calculate average of car prices
+    }
 
-  }
+    @Test
+    public void test() throws Exception {
 
-  @Test
-  public void test() throws Exception {
-
-  }
+    }
 }
 
 
